a518a6601cce384760ca5aa3bc0925295c990858
javadoterr <javadoterr@gmail.com>
2026-05-26 20:39:56 +0530

    log added

--- src/main/java/com/javadoterr/api/controller/TestController.java
+++ src/main/java/com/javadoterr/api/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.javadoterr.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(path = "/v1/test")
+@Slf4j
 public class TestController {
 
     @GetMapping(path = "/hello")
     public ResponseEntity<?> greeting(){
+        log.info("CONTROLLER : received request : METHOD : GET");
         return ResponseEntity.ok().body("Hey! This is test message : "
                 + LocalDateTime.now().toString());
     }
